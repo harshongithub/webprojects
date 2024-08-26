c3758ba3a0d63287b0a849f848d29d4741174130
Harsh Panwar <harshpanwar462@gmail.com>
2024-08-26 22:52:55 +0530

    create table

--- server.py
+++ server.py
@@ -13,9 +13,6 @@ db = SQLAlchemy(model_class=Base)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///contact-list.db")
 db.init_app(app)
 
-with app.app_context():
-    db.drop_all()  # Drops all tables
-    db.create_all()  # Creates all tables based on the updated model
 
 class Contact(db.Model):
     __tablename__ = 'contact'  # optional: specify table name
